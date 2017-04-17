2b25a43cc311714d1262e5f3e826eca9b634b138
Logan Patino <loganpatino10@gmail.com>
2017-04-17 10:42:15 -0400

    Add functions to get labels for specific keys of the data

--- run.py
+++ run.py
@@ -1,13 +1,21 @@
 from lyric_data import getSplitupLyricData
 from lyric_features import setFeaturesBoW
+from lyric_labels import getLabels
 
 if __name__ == '__main__':
     data = getSplitupLyricData()
+
+    # gets desired labels for artist classification while also modifying
+    # data to get rid of unseen artists in test and validation sets
+    labels = getLabels(data, 'artist')
+
     res = setFeaturesBoW(data) # {cv, train, test, validation}
     cv = res["cv"] # the CountVectorizer
     train_counts = res["train"]
     validate_counts = res["validation"]
     test_counts = res["test"]
+
+
 
     print train_counts.shape
     print validate_counts.shape
